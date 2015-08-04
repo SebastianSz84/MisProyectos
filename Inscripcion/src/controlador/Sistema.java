9d3d788f94f4d6ddb0c5535c3e90b6f567750d18
SebastianSz84 <sebastiansz@gmail.com>
2015-08-03 23:26:08 -0300

    Append Inscripcion

---
+++ Inscripcion/src/controlador/Sistema.java
@@ -0,0 +1,95 @@
+package controlador;
+
+import java.util.Vector;
+
+import VO.AlumnoVO;
+import VO.CursoVO;
+import VO.MateriaVO;
+import VO.ProfesorVO;
+
+public class Sistema {
+	public void agregarAlumno(AlumnoVO alumno){
+		
+	}
+
+	public void agregarMateria(MateriaVO materia){
+		
+	}
+
+	public void agregarProfesor(ProfesorVO profesor){
+		
+	}
+
+	public void nuevoCurso(MateriaVO materia, ProfesorVO profesor, int cantidadMaxima){
+		
+	}
+
+	public void eliminarAlumno(AlumnoVO alumno){
+		
+	}
+
+	public void eliminarMateria(MateriaVO materia){
+		
+	}
+
+	public void eliminarCurso(CursoVO curso){
+
+	}
+
+	public void eliminarProfesor(ProfesorVO profesor){
+		
+	}
+
+	public AlumnoVO obtenerAlumnoPorLegajo(AlumnoVO alumno){
+		return null;
+		
+	}
+
+	public CursoVO obtenerCursoPorNumero(CursoVO curso){
+		return null;
+		
+	}
+
+	public MateriaVO obtenerMateriaPorNumero(MateriaVO materia){
+		return null;
+		
+	}
+
+	public ProfesorVO obtenerProfesorPorLegajo(ProfesorVO profesor){
+		return null;
+		
+	}
+
+	public Vector<AlumnoVO> obtenerAlumnos(){
+		return null;
+		
+	}
+
+	public Vector<CursoVO> obtenerCursos(){
+		return null;
+		
+	}
+
+	public Vector<MateriaVO> obtenerMaterias(){
+		return null;
+		
+	}
+
+	public Vector<ProfesorVO> obtenerProfesores(){
+		return null;
+		
+	}
+
+	public void asignarMateriaProfesor(MateriaVO materia, ProfesorVO profesor){
+		
+	}
+
+	public void asignarAlumnoCurso(CursoVO curso, AlumnoVO alumno){
+		
+	}
+
+	public void eliminarAlumnoCurso(CursoVO curso, AlumnoVO alumno){
+		
+	}
+
+}
